70535e0d294ff085110d4d06218702b466aac20b
guywwww <luay1545@gmail.com>
2026-04-18 03:36:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 
-mongoose.connect("YOUR_MONGODB_URI");
+mongoose.connect("mongodb+srv://HoodStrikeBot:Dante69195999@cluster0.giyufvh.mongodb.net/hoodstrike?retryWrites=true&w=majority")
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log("Mongo Error:", err));
 
 const Ban = mongoose.model("Ban", {
     username: String,
@@ -13,28 +15,42 @@ const Ban = mongoose.model("Ban", {
 
 let actions = [];
 
+app.get("/", (req, res) => {
+    res.send("OK");
+});
+
 app.post("/ban", async (req, res) => {
-    const { username, reason } = req.body;
+    try {
+        const { username, reason } = req.body;
 
-    await Ban.findOneAndUpdate(
-        { username },
-        { username, reason },
-        { upsert: true }
-    );
+        await Ban.findOneAndUpdate(
+            { username },
+            { username, reason },
+            { upsert: true }
+        );
 
-    actions.push({ type: "ban", username, reason });
+        actions.push({ type: "ban", username, reason });
 
-    res.sendStatus(200);
+        res.sendStatus(200);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
 });
 
 app.post("/unban", async (req, res) => {
-    const { username } = req.body;
+    try {
+        const { username } = req.body;
 
-    await Ban.deleteOne({ username });
+        await Ban.deleteOne({ username });
 
-    actions.push({ type: "unban", username });
+        actions.push({ type: "unban", username });
 
-    res.sendStatus(200);
+        res.sendStatus(200);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
 });
 
 app.post("/kick", (req, res) => {
@@ -46,11 +62,16 @@ app.post("/kick", (req, res) => {
 });
 
 app.get("/check/:username", async (req, res) => {
-    const user = await Ban.findOne({ username: req.params.username });
+    try {
+        const user = await Ban.findOne({ username: req.params.username });
 
-    if (!user) return res.json({ banned: false });
+        if (!user) return res.json({ banned: false });
 
-    res.json({ banned: true, reason: user.reason });
+        res.json({ banned: true, reason: user.reason });
+    } catch (e) {
+        console.log(e);
+        res.json({ banned: false });
+    }
 });
 
 app.get("/poll", (req, res) => {
@@ -59,4 +80,4 @@ app.get("/poll", (req, res) => {
     res.json(data);
 });
 
-app.listen(3000);
+app.listen(3000, () => console.log("Server running on port 3000"));
